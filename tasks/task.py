b4daef793abd5a39c46ddfe211385a9b31f30afe
Salimatou <salimatou.traore@edu.esiee.fr>
2023-10-16 17:30:13 +0200

    Add Docstring

--- tasks/task.py
+++ tasks/task.py
@@ -4,6 +4,13 @@ class Task:
     _id_counter = 0
 
     def __init__(self, name, description):
+        """
+        Initializes a Task object with a unique ID, name, description, completion status (False), and creation date (current datetime).
+
+        Args:
+            name (str): The name of the task.
+            description (str): The description of the task.
+        """
         self.id = Task._id_counter
         Task._id_counter += 1
         self.name = name
@@ -12,8 +19,17 @@ class Task:
         self.created_date = datetime.now()
 
     def mark_completed(self):
+        """
+        Marks the task as completed by setting the completion status to True.
+        """
         self.completed = True
 
     def __str__(self):
+        """
+        Returns a string representation of the task, including its ID, name, description, completion status, and creation date.
+
+        Returns:
+            str: A formatted string representation of the task.
+        """
         status = "Terminée" if self.completed else "Non terminée"
         return f"ID : {self.id}\nTâche : {self.name}\nDescription : {self.description}\nStatut : {status}\nDate de création : {self.created_date}\n"
