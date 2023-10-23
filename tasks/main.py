708794c407adaa3cbbaced55157eda756164bbcf
pralban <alban.pereira@telecom-paris.fr>
2023-10-23 18:49:20 +0200

    update logs

--- tasks/main.py
+++ tasks/main.py
@@ -1,36 +1,9 @@
 from .tasklist import TaskList
+from logs import configure_logging
+
 import logging
 
-# Configuration pour les informations
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s [INFO] %(message)s',
-    handlers=[
-        # Affiche les INFOS dans le fichier app.log
-        logging.FileHandler('logs/info.log'),
-        # logging.StreamHandler() #Affiche les INFOS dans le terminal
-    ]
-)
-
-# Configuration pour le débogage
-# Utilisez le même nom ici (avec un underscore)
-debug_logger = logging.getLogger("debug_logger")
-debug_logger.setLevel(logging.DEBUG)
-debug_handler = logging.FileHandler('logs/debug.log')
-debug_handler.setLevel(logging.DEBUG)
-debug_formatter = logging.Formatter('%(asctime)s [DEBUG] %(message)s')
-debug_handler.setFormatter(debug_formatter)
-debug_logger.addHandler(debug_handler)
-
-# Configuration pour les erreurs
-error_logger = logging.getLogger("error_logger")
-error_logger.setLevel(logging.ERROR)
-error_handler = logging.FileHandler('logs/error.log')
-error_handler.setLevel(logging.ERROR)
-error_formatter = logging.Formatter('%(asctime)s [ERROR] %(message)s')
-error_handler.setFormatter(error_formatter)
-error_logger.addHandler(error_handler)
-
+configure_logging()
 
 def display_menu():
     print("\nMenu :")
