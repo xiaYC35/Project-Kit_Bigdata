30a83f374e3edb78f3e1a32a5b4e01569d29cc98
Aurelienraulo <aurelien.raulo@telecom-paris.fr>
2023-10-25 11:13:07 +0200

    Modify docstrings to respect docstyle

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,17 @@
+"""Initialize documentation Sphinx."""
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
+# -- Informations sur le projet -----------------------------------------------------
 project = 'Projet-Kit_Bigdata'
 copyright = '2023, Alban Pereira - Yuchen Xia - Aurélien Raulo - Salimatou Traore'
 author = 'Alban Pereira - alban.pereira98@gmail.com & Yuchen Xia - xiayuchen35@gmail.com & Aurélien Raulo - aurelien0raulo@gmail.com & Salimatou Traore - tra.salimatou@gmail.com'
 release = '1.0.0'
 
-# -- General configuration ---------------------------------------------------
+# -- Configuration générale ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
@@ -22,9 +19,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode'
 templates_path = ['furo']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
-# -- Options for HTML output -------------------------------------------------
+# -- Options pour la sortie HTML -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
