373aa566f8553436ae68ff1953183bba480d674a
xiaYC35 <xiayuchen35@gmail.com>
2023-10-27 12:11:46 +0200

    1.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 # Les données de projet
 setup(
     name='my_task_manager',
-    version='1.5',
+    version='1.6',
     description='Métadonnées et informations sur la construction de projet To-do List',
     author='Alban Pereira, Yuchen Xia, Aurélien Raulo, Salimatou Traore',
     author_email='alban.pereira98@gmail.com, xiayuchen35@gmail.com, aurelien0raulo@gmail.com, tra.salimatou@gmail.com',
