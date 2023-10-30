57720d96189be114dbe51dc0eeaf8c0a05dbc494
pralban <alban.pereira@telecom-paris.fr>
2023-10-30 12:22:21 +0100

    fix pep8 problems on test_task

--- tests/test_task.py
+++ tests/test_task.py
@@ -60,7 +60,9 @@ class TestCodeUnderTest:
         """
         task = Task("Task 1", "Description 1")
         id = task.id
-        expected_output = "ID : " + str(id) + "\nTâche : Task 1\nDescription : Description 1\nStatut : Non terminée\nDate de création : " + str(task.created_date) + "\n"
+        expected_output = "ID : " + \
+            str(id) + "\nTâche : Task 1\nDescription : Description 1\nStatut : Non terminée\nDate de création : " + \
+            str(task.created_date) + "\n"
         assert str(task) == expected_output
 
     # create multiple tasks and check if their IDs are unique
